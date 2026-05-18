e52d285edd53c49bbec4a00af48942241d757d77
Issachar <s0504850418@gmail.com>
2026-05-18 19:51:39 +0300

    kj;hlknvX

--- api/v1/controllrs/product.js
+++ api/v1/controllrs/product.js
@@ -1,32 +1,57 @@
 const mysqlDb=require('../modoels/mysqldb');
 module.exports={
+    
+        getAll:(req,res)=>{
+          const sql='SELECT * FROM t_product';
+           mysqlDb.query(sql,(err,results,feilds)=>{
+              if(err==null)
+                  {
+                      console.log(results);
+                      res.status(200).json(results);
+               }
+               else
+                  {
+                      console.log(err);
+                      res.status(500).json({'error': err.message});
+                  }
+              });
+     },
 
-getAll:(req,res)=>{
-    const sql='SELECT * FROM t_product';
-    mysqlDb.query(sql,(err,results,feilds)=>{
-    if(err==null)
-    {
-        console.log(results);
-        res.status(200).json(results);
-    }
-    else
-    {
-        console.log(err);
-        res.status(500).json({'error': err.message});
-    }
-});
 
-},
+        getById:(req,res)=>{
+            const pid=req.params.id;
+            const sql=`SELECT * FROM t_product WHERE pid=${pid}`;
 
-getById:(req,res)=>{
-const uid=req.params.id;
-res.status(200).json({msg:`get user id ${uid}`});
-},
+            mysqlDb.query(sql,(err,results,feilds)=>{
+                if(err==null)
+                    {
+                        console.log(results);
+                        res.status(200).json(results);
+                    }
+                    else
+                    {
+                        console.log(err);
+                        res.status(500).json({'error': err.message});
+                    }
+                });
+            },
 
 delete:(req,res)=>{
-const uid=req.params.id;
-res.status(200).json({msg:`delete user id ${uid}`});
-},
+const pid=req.params.pid;
+const sql=`DELETE FROM t_product WHERE pid=${pid}`;
+ mysqlDb.query(sql,(err,results,feilds)=>{
+                if(err==null)
+                    {
+                        console.log(results);
+                        res.status(200).json(results);
+                    }
+                    else
+                    {
+                        console.log(err);
+                        res.status(500).json({'error': err.message});
+                    }
+                });
+            },
 
 update:(req,res)=>{
 const uid=req.params.id;
--- api/v1/routes/product.js
+++ api/v1/routes/product.js
@@ -3,12 +3,12 @@ const productController=require('../controllrs/product');
 
 router.get('/',productController.getAll);
 
-router.get('/:id',productController.getById);
+router.get('/:pid',productController.getById);
 
-router.delete('/:id',productController.delete);
+router.delete('/:pid',productController.delete);
 
 router.post('/',productController.AddNew);
 
-router.put('/:id',productController.update);
+router.put('/:pid',productController.update);
 
 module.exports=router;
